80c53de87a68f988c795cf54189b80a0a23cb2bf
mfg637 <mfg636@gmail.com>
2025-03-25 00:04:19 +0200

    calculate representations (+ tests)

--- image_processing/common/compatibility_level.py
+++ image_processing/common/compatibility_level.py
@@ -40,7 +40,49 @@ FORMAT_LEVEL: dict[FormatEnum, int] = {
     FormatEnum.SVG: 0
 }
 
+ENCODING_FORMAT_BY_LEVEL: dict[int, FormatEnum] = {
+    0: FormatEnum.AVIF,
+    1: FormatEnum.AVIF,
+    2: FormatEnum.AVIF,
+    3: FormatEnum.WEBP,
+    4: FormatEnum.JPEG
+}
+
 def get_image_compatibility_level(size: tuple[int, int], file_format: FormatEnum) -> int:
     level_by_size = get_compatibility_level_by_size(*size)
     level_by_format = FORMAT_LEVEL[file_format]
     return min(level_by_size, level_by_format)
+
+def calc_representations_format(size: tuple[int, int], file_format: FormatEnum)\
+        -> dict[int, tuple[FormatEnum, int, int] | None]:
+    result = {
+        0: None,
+        1: None,
+        2: None,
+        3: None,
+        4: None
+    }
+    level_by_size = get_compatibility_level_by_size(*size)
+    level_by_format = FORMAT_LEVEL[file_format]
+    current_level = 0
+    current_size = size
+    size_divider = 2
+    if level_by_format < level_by_size:
+        result[level_by_format] = (file_format, size[0], size[1])
+        result[level_by_size] = (ENCODING_FORMAT_BY_LEVEL[level_by_size], size[0], size[1])
+        current_level = level_by_size
+    else:
+        current_format = file_format
+        if file_format == FormatEnum.PNG:
+            current_format = ENCODING_FORMAT_BY_LEVEL[level_by_size]
+        current_level = level_by_size
+        result[current_level] = (current_format, size[0], size[1])
+    while current_level < 4:
+        current_level += 1
+        while get_compatibility_level_by_size(*current_size) < current_level:
+            current_size = (round(size[0] / size_divider), round(size[1] / size_divider))
+            size_divider *= 2
+        result[current_level] = (
+            ENCODING_FORMAT_BY_LEVEL[current_level], current_size[0], current_size[1]
+        )
+    return result
--- image_processing/tests/compatibility_level.py
+++ image_processing/tests/compatibility_level.py
@@ -49,3 +49,81 @@ class CompatiblityLevelTest(SimpleTestCase):
         self.assertEqual(cl.get_image_compatibility_level(size, ff.FormatEnum.JPEG), 0)
         self.assertEqual(cl.get_image_compatibility_level(size, ff.FormatEnum.WEBP), 0)
         self.assertEqual(cl.get_image_compatibility_level(size, ff.FormatEnum.AVIF), 0)
+
+    def test_respesentations_from_cl0_png(self):
+        width = 29301
+        height = 19321
+        size = (width, height)
+        representations = cl.calc_representations_format(size, ff.FormatEnum.PNG)
+        self.assertEqual(
+            representations[0],
+            (ff.FormatEnum.AVIF, width, height)
+        )
+        self.assertEqual(
+            representations[1],
+            (ff.FormatEnum.AVIF, 7325, 4830)
+        )
+        self.assertEqual(
+            representations[2],
+            (ff.FormatEnum.AVIF, 3663, 2415)
+        )
+        self.assertEqual(
+            representations[3],
+            (ff.FormatEnum.WEBP, 1831, 1208)
+        )
+        self.assertEqual(
+            representations[4],
+            (ff.FormatEnum.JPEG, 916, 604)
+        )
+    
+    def test_respesentations_from_cl0_jpeg(self):
+        width = 29301
+        height = 19321
+        size = (width, height)
+        representations = cl.calc_representations_format(size, ff.FormatEnum.JPEG)
+        self.assertEqual(
+            representations[0],
+            (ff.FormatEnum.JPEG, width, height)
+        )
+        self.assertEqual(
+            representations[1],
+            (ff.FormatEnum.AVIF, 7325, 4830)
+        )
+        self.assertEqual(
+            representations[2],
+            (ff.FormatEnum.AVIF, 3663, 2415)
+        )
+        self.assertEqual(
+            representations[3],
+            (ff.FormatEnum.WEBP, 1831, 1208)
+        )
+        self.assertEqual(
+            representations[4],
+            (ff.FormatEnum.JPEG, 916, 604)
+        )
+    
+    def test_representatons_from_svg(self):
+        width = 7593
+        height = 1246
+        size = (width, height)
+        representations = cl.calc_representations_format(size, ff.FormatEnum.SVG)
+        self.assertEqual(
+            representations[0],
+            (ff.FormatEnum.SVG, width, height)
+        )
+        self.assertEqual(
+            representations[1],
+            (ff.FormatEnum.AVIF, width, height)
+        )
+        self.assertEqual(
+            representations[2],
+            (ff.FormatEnum.AVIF, 3796, 623)
+        )
+        self.assertEqual(
+            representations[3],
+            (ff.FormatEnum.WEBP, 1898, 312)
+        )
+        self.assertEqual(
+            representations[4],
+            (ff.FormatEnum.JPEG, 949, 156)
+        )
