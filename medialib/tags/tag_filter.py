65b8d4481a3d03c283a73714abf1a8982a941e58
mfg637 <mfg636@gmail.com>
2026-05-25 19:56:29 +0300

    remove "ai generated" tag from aesthetic filter

--- medialib/tags/tag_filter.py
+++ medialib/tags/tag_filter.py
@@ -31,7 +31,7 @@ def aesthetic_filter(
     queryset: QuerySet[ml_models.Content],
 ) -> QuerySet[ml_models.Content]:
     q_object = TagDSLParser(
-        "safe & !(vector | sketch | simple background | screencap | photo | ai:generated | comic)"
+        "safe & !(vector | sketch | simple background | screencap | photo | comic)"
     ).parse()
     return basic_filter(queryset).filter(q_object)
 
